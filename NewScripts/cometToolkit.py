b990ed406759b415345c87ca56ad8ff639317198
daraghhhollman <hollmandaragh@gmail.com>
2024-03-19 23:01:38 +0000

    Full image reduction pipeline complete

--- NewScripts/cometToolkit.py
+++ NewScripts/cometToolkit.py
@@ -21,13 +21,19 @@ def GetImage(path):
     return fits.open(path)[0].data
 
 
-def SaveFits(data, outputPath):
+def SaveFits(data, outputPath, header=None):
     """
     Saving fits files
     """
 
-    newFits = fits.PrimaryHDU()
-    newFits.data = data
+    newFits = fits.PrimaryHDU(data=data)
+    #newFits.data = data.astype(int)
+
+    print(np.max(data))
+    print(np.max(newFits.data))
+
+    if header is not None:
+        newFits.header = header
 
     newFits.writeto(outputPath, overwrite=True)
     print(f"FITS image saved to: {outputPath}")
@@ -75,7 +81,40 @@ def PlotFits(path, wcsPath="", vmin=None, vmax=None,
         plt.xlabel("CCD X")
         plt.ylabel("CCD Y")
 
-    
+
+##### IMAGE MANIPULATION #####
+
+def RollImage(image, rollTuple):
+    return np.roll(image, rollTuple, axis=(1,0))
+
+
+
+###### IMAGE REDUCTION #######
+
+def ReduceImage(inputPath, outputPath, biasPath, darkPath, flatPath, showPlot=False):
+
+    # Object image minus bias minus darks (bias subtracted) and divided by the master flats
+
+    fitsFile = fits.open(inputPath)
+    biasImage = fits.open(biasPath)[0].data
+    darkImage = fits.open(darkPath)[0].data
+    flatImage = fits.open(flatPath)[0].data
+
+    objectImage = fitsFile[0].data
+    header = fitsFile[0].header
+
+    reducedImage = (objectImage - biasImage - darkImage) / flatImage
+
+    #SaveFits(reducedImage, outputPath, header=header)
+
+    newFits = fits.PrimaryHDU(data=reducedImage)
+    newFits.writeto(outputPath, overwrite=True)
+
+    if showPlot:
+        plt.imshow(reducedImage, vmax=16000)
+
+    return
+
 
 ###### STACKING ########
 
@@ -105,22 +144,21 @@ def FindCometCentre(path, filter, maxCentreDistance=500,
     # FInd all sources in sky
     sources = SearchStars(image, fwhm=8, threshold=16, showPlot=False)
 
-
     # Remove sources based on peak counts and distance from centre
 
     # Values calibrated manually per filter
     match filter:
         case "V":
-            maxPeak = 2400
-            minPeak = 1800
+            maxPeak = 1400
+            minPeak = 700
 
         case "R":
-            maxPeak = 3200
+            maxPeak = 2400
             minPeak = 2000
 
         case "B":
-            maxPeak = 1350
-            minPeak = 1200
+            maxPeak = 1000
+            minPeak = 200
 
 
     removeIndices = []
@@ -143,21 +181,20 @@ def FindCometCentre(path, filter, maxCentreDistance=500,
         
         match filter:
             case "V":
-                upperThreshold = 1600
-                lowerThreshold = 1150
+                upperThreshold = 1200
+                lowerThreshold = 200
 
             case "R":
-                upperThreshold = 1200
-                lowerThreshold = 1040
+                upperThreshold = 1000
+                lowerThreshold = 500
 
             case "B":
-                upperThreshold = 1200
-                lowerThreshold = 1040
+                upperThreshold = 1000
+                lowerThreshold = 200
 
         thresholdIndices = np.where((image > lowerThreshold)  & (image < upperThreshold), 1, 0)
 
         plt.imshow(thresholdIndices)
-
 
         for source in sources:
             plt.scatter(source["xcentroid"], source["ycentroid"])
@@ -190,6 +227,19 @@ def FindCometCentre(path, filter, maxCentreDistance=500,
 
 
 
+
+def MedianStack(imagesList, preserveAvgCounts=False):
+    """
+    Correctly median stacks a list of images by multiplying by the sqrt of the number of images
+    """
+
+    if preserveAvgCounts:
+        return np.median(imagesList, axis=0)
+
+    else:
+        return np.median(imagesList, axis=0) * np.sqrt(len(imagesList))
+
+
 def StackImages(paths):
 
     imagesToStack = []
@@ -200,7 +250,7 @@ def StackImages(paths):
 
         imagesToStack.append(image)
 
-    stackedImage = np.median(imagesToStack, axis=0) * np.sqrt(len(paths))
+    stackedImage = MedianStack(imagesToStack, preserveAvgCounts=True)
 
     return stackedImage
 
